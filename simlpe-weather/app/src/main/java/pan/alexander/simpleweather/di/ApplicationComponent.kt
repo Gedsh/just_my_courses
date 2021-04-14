9ba12e48cbc05e171155d693027db3f6ac9b1d94
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-04-14 18:45:27 +0300

    Add MainInteractor to Dagger

--- simlpe-weather/app/src/main/java/pan/alexander/simpleweather/di/ApplicationComponent.kt
+++ simlpe-weather/app/src/main/java/pan/alexander/simpleweather/di/ApplicationComponent.kt
@@ -6,11 +6,13 @@ import pan.alexander.simpleweather.data.CurrentWeatherRepositoryImpl
 import pan.alexander.simpleweather.data.database.AppDatabase
 import pan.alexander.simpleweather.data.database.CurrentWeatherDao
 import pan.alexander.simpleweather.data.web.CurrentWeatherApiService
+import pan.alexander.simpleweather.domain.MainInteractor
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [RepositoryModule::class, AppContextModule::class, RoomModule::class, RetrofitModule::class])
 interface ApplicationComponent {
+    fun getInteractor(): MainInteractor
 
     fun getRepository(): CurrentWeatherRepositoryImpl
 
