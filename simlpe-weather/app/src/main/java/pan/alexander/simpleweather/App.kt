66adecc425a2afb1e286f2ee3cb115d19d5e0a75
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-04-14 18:35:20 +0300

    Add image URL and tag for the logs

--- simlpe-weather/app/src/main/java/pan/alexander/simpleweather/App.kt
+++ simlpe-weather/app/src/main/java/pan/alexander/simpleweather/App.kt
@@ -8,6 +8,8 @@ class App : Application() {
     companion object{
         lateinit var instance: App
         const val BASE_URL = "https://api.openweathermap.org/"
+        const val BASE_IMAGE_URL = "http://openweathermap.org/"
+        const val LOG_TAG = "SimpleWeather"
     }
 
     lateinit var daggerComponent: ApplicationComponent
