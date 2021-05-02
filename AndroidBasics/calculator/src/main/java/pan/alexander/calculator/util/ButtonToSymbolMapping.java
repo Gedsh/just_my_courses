0cf478206f223eb29263bbc1f2944c9d171205de
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-05-03 02:33:07 +0300

    Add button to symbol mapping

---
+++ AndroidBasics/calculator/src/main/java/pan/alexander/calculator/util/ButtonToSymbolMapping.java
@@ -0,0 +1,29 @@
+package pan.alexander.calculator.util;
+
+public interface ButtonToSymbolMapping {
+    String buttonOne = "1";
+    String buttonTwo = "2";
+    String buttonThree = "3";
+    String buttonFour = "4";
+    String buttonFive = "5";
+    String buttonSix = "6";
+    String buttonSeven = "7";
+    String buttonEight = "8";
+    String buttonNine = "9";
+    String buttonZero = "0";
+    String buttonDivide = "/";
+    String buttonMultiply = "*";
+    String buttonMinus = "-";
+    String buttonPlus = "+";
+    String buttonPercent = "%";
+    String buttonSQRT = "SQRT(";
+    String buttonPowered = "^";
+    String buttonEquals = "=";
+    String buttonPoint = ".";
+    String buttonClear = "CLEAR";
+    String buttonBackspace = "BACKSPACE";
+    String buttonBracketsOpen = "(";
+    String buttonBracketsClose = ")";
+    String buttonHistory = "HISTORY";
+    String buttonAux = "AUX";
+}
