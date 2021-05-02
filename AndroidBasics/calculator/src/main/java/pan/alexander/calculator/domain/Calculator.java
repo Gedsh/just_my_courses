f2ba7899930c3ce853c1ad3e5081adf3ef2f18c6
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-05-03 02:26:57 +0300

    Add class for evaluating expressions

---
+++ AndroidBasics/calculator/src/main/java/pan/alexander/calculator/domain/Calculator.java
@@ -0,0 +1,49 @@
+package pan.alexander.calculator.domain;
+
+import android.util.Log;
+
+import com.udojava.evalex.Expression;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import static pan.alexander.calculator.App.LOG_TAG;
+
+public class Calculator {
+    private final Map<String, String> symbolToExpressionMap;
+
+    @Inject
+    public Calculator() {
+        symbolToExpressionMap = new HashMap<>();
+
+        fillUnicodeToExpressionMap();
+    }
+
+    private void fillUnicodeToExpressionMap() {
+        symbolToExpressionMap.put("\\%", "/100");
+    }
+
+    public String calculateExpression(String expressionLine) {
+
+        expressionLine = convertExpressionLineToMathExpression(expressionLine);
+
+        String result = "";
+        try {
+            Expression expression = new Expression(expressionLine);
+            result = expression.eval(true).toString();
+        } catch (Exception e) {
+            Log.w(LOG_TAG, "Calculator exception " + e.getMessage());
+        }
+
+        return result;
+    }
+
+    private String convertExpressionLineToMathExpression(String expressionLine) {
+        for (Map.Entry<String, String> entry: symbolToExpressionMap.entrySet()) {
+            expressionLine = expressionLine.replaceAll(entry.getKey(), entry.getValue());
+        }
+        return expressionLine;
+    }
+}
