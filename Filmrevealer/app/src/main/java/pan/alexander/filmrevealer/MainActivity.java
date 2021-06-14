58c34e0ae39a76099e3a973d5a818f3e421f0102
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-06-15 00:21:13 +0300

    Initial commit

---
+++ Filmrevealer/app/src/main/java/pan/alexander/filmrevealer/MainActivity.java
@@ -0,0 +1,21 @@
+package pan.alexander.filmrevealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pan.alexander.filmrevealer.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ Filmrevealer/app/src/main/java/pan/alexander/filmrevealer/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package pan.alexander.filmrevealer.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
