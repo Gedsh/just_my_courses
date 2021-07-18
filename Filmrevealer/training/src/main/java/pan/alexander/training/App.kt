105c9c11a858d9547e26b24157754d4a23a66761
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-07-18 14:54:15 +0300

    Lesson11: Add push notifications

--- Filmrevealer/training/src/main/java/pan/alexander/training/App.kt
+++ Filmrevealer/training/src/main/java/pan/alexander/training/App.kt
@@ -1,7 +1,13 @@
 package pan.alexander.training
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import pan.alexander.training.di.*
+import pan.alexander.training.services.TrainingFirebaseMessagingService.Companion.FIREBASE_CHANNEL_ID
 
 class App : Application() {
     companion object {
@@ -17,6 +23,10 @@ class App : Application() {
         instance = this
 
         initDaggerComponent()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createFirebaseNotificationChannel()
+        }
     }
 
     private fun initDaggerComponent() {
@@ -26,5 +36,18 @@ class App : Application() {
             .contentResolverModule(ContentResolverModule(instance))
             .locationModule(LocationModule(instance))
             .build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createFirebaseNotificationChannel() {
+        val notificationManager =
+            ContextCompat.getSystemService(this, NotificationManager::class.java)
+        val name = getString(R.string.firebase_channel_name)
+        val descriptionText = getString(R.string.firebase_channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(FIREBASE_CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager?.createNotificationChannel(channel)
     }
 }
--- Filmrevealer/training/src/main/java/pan/alexander/training/presentation/fragments/NotificationsFragment.kt
+++ Filmrevealer/training/src/main/java/pan/alexander/training/presentation/fragments/NotificationsFragment.kt
@@ -1,45 +1,86 @@
 package pan.alexander.training.presentation.fragments
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import pan.alexander.training.databinding.FragmentNotificationsBinding
-import pan.alexander.training.presentation.viewmodels.NotificationsViewModel
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                if (it.action == FIREBASE_ACTION) {
+                    it.getStringExtra(EXTRA_PARAM_TITLE)
+                        ?.takeIf { title -> title.isNotBlank() }
+                        ?.let { title ->
+                            binding.firebaseMessageTitle.text = title
+                        }
+                    it.getStringExtra(EXTRA_PARAM_MESSAGE)
+                        ?.takeIf { message -> message.isNotBlank() }
+                        ?.let { message ->
+                            binding.firebaseMessage.text = message
+                        }
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
+    ): View {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerFirebaseBroadcastReceiver()
+    }
+
+    private fun registerFirebaseBroadcastReceiver() {
+        context?.let {
+            LocalBroadcastManager.getInstance(it)
+                .registerReceiver(broadcastReceiver, IntentFilter(FIREBASE_ACTION))
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+
+        unregisterFirebaseBroadcastReceiver()
+
         _binding = null
+    }
+
+    private fun unregisterFirebaseBroadcastReceiver() {
+        context?.let {
+            LocalBroadcastManager.getInstance(it)
+                .unregisterReceiver(broadcastReceiver)
+        }
+    }
+
+    companion object {
+        const val FIREBASE_ACTION =
+            "pan.alexander.training.presentation.fragments.action.FIREBASE_ACTION"
+
+        const val EXTRA_PARAM_TITLE =
+            "pan.alexander.training.presentation.fragments.extra.PARAM_TITLE"
+
+        const val EXTRA_PARAM_MESSAGE =
+            "pan.alexander.training.presentation.fragments.extra.PARAM_MESSAGE"
     }
 }
