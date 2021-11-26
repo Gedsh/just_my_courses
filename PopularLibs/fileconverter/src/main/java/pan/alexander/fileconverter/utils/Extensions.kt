2ca3e4f3ab8321923f4dd04b4a9401ad51e0bead
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-11-26 10:40:29 +0200

    Remove old fileconverter

--- PopularLibs/fileconverter/src/main/java/pan/alexander/fileconverter/utils/Extensions.kt
+++
@@ -1,6 +0,0 @@
-package pan.alexander.fileconverter.utils
-
-import android.content.Context
-import pan.alexander.fileconverter.App
-
-val Context.app get() = applicationContext as App
--- PopularLibs/fileconverter/src/main/java/pan/alexander/fileconverter/utils/rxschedulers/SchedulersProvider.kt
+++
@@ -1,9 +0,0 @@
-package pan.alexander.fileconverter.utils.rxschedulers
-
-import io.reactivex.rxjava3.core.Scheduler
-
-interface SchedulersProvider {
-    fun io(): Scheduler
-    fun computation(): Scheduler
-    fun ui(): Scheduler
-}
